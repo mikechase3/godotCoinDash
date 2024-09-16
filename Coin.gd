f4946afc28631aeb1ff942a3833593aa36265d01
Michael Chase Jr <protectmikechase@gmail.com>
2024-09-16 01:24:29 -0400

    changed fps of coin to 12

---
+++ Coin.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@export var speed = 350
+var velocity = Vector2.ZERO
+var screensize = Vector2(480, 720)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
